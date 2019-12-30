d0bcc737396d21932a539c1bb3b797f03198da3e
Alan <arvinHan@foxmail.com>
2019-12-30 20:06:09 +0800

    Fixed 运行时Plugins出错

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Saas_ihrm
-基于Saas的IHRM系统实现
+# 基于Saas的IHRM系统
+
+## 开发环境
+
+* JDK : Oracle JDK 1.8
+* mysql ： 5.7
+* Maven version ： 3.5.2
+
+
+
+## 前后端技术
+
+* 前端 : Vue + element-ui
+* 后端 ： SpringBoot + SpringCloud + SpringMVC + SpringData
--- ihrm_parent/ihrm_company/src/main/java/com/ihrm/company/controller/CompanyController.java
+++ ihrm_parent/ihrm_company/src/main/java/com/ihrm/company/controller/CompanyController.java
@@ -51,11 +51,11 @@ public class CompanyController {
     //根据id查询企业
     @RequestMapping(value="/{id}",method = RequestMethod.GET)
     public Result findById(@PathVariable(value="id") String id) throws CommonException {
-        throw new CommonException(ResultCode.UNAUTHORISE);
-//        Company company = companyService.findById(id);
-//        Result result = new Result(ResultCode.SUCCESS);
-//        result.setData(company);
-//        return result;
+        //throw new CommonException(ResultCode.UNAUTHORISE);
+        Company company = companyService.findById(id);
+        Result result = new Result(ResultCode.SUCCESS);
+        result.setData(company);
+        return result;
     }
 
     //查询全部企业列表
