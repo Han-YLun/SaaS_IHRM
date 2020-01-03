791d40747ff69e08f509cd7b67df02d1a6513fb4
Alan <arvinhan@foxmail.com>
2020-01-03 21:37:03 +0800

    Added 部门Dao和Service

---
+++ ihrm_parent/ihrm_common_model/src/main/java/com/ihrm/domain/company/Department.java
@@ -0,0 +1,62 @@
+package com.ihrm.domain.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * (Department)实体类
+ */
+@Entity
+@Table(name = "co_department")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department implements Serializable {
+    private static final long serialVersionUID = -9084332495284489553L;
+    //ID
+    @Id
+    private String id;
+    /**
+     * 父级ID
+     */
+    private String pid;
+    /**
+     * 企业ID
+     */
+    private String companyId;
+    /**
+     * 部门名称
+     */
+    private String name;
+    /**
+     * 部门编码，同级部门不可重复
+     */
+    private String code;
+
+    /**
+     * 负责人ID
+     */
+    private String managerId;
+    /**
+	*  负责人名称
+	*/
+    private String manager;
+
+    /**
+     * 介绍
+     */
+    private String introduce;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+}
---
+++ ihrm_parent/ihrm_company/src/main/java/com/ihrm/company/service/DepartmentService.java
@@ -0,0 +1,72 @@
+package com.ihrm.company.service;
+
+import com.ihrm.common.utils.IdWorker;
+import com.ihrm.company.dao.DepartmentDao;
+import com.ihrm.domain.company.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author: hyl
+ * @date: 2020/01/03
+ **/
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    @Autowired
+    private IdWorker idWorker;
+
+    /**
+     * 保存部门
+     */
+    public void save(Department department){
+        //设置主键
+        String id = idWorker.nextId() + "";
+        department.setId(id);
+        //调用dao保存部门
+        departmentDao.save(department);
+    }
+
+    /**
+     * 更新部门
+     */
+    public void update(Department department){
+        //根据id查询部门
+        Department dept = departmentDao.findById(department.getId()).get();
+        //设置部门属性
+        dept.setCode(department.getCode());
+        dept.setIntroduce(department.getIntroduce());
+        dept.setName(department.getName());
+        //更新部门
+        departmentDao.save(dept);
+    }
+
+
+    /**
+     * 根据id查询部门
+     */
+    public Department findById(String id){
+        return departmentDao.findById(id).get();
+    }
+
+
+    /**
+     * 查询全部部门列表
+     */
+    public List<Department> findAll(){
+        return departmentDao.findAll();
+    }
+
+    /**
+     * 根据id删除部门
+     */
+    public void deleteById(String id){
+        departmentDao.deleteById(id);
+    }
+}
