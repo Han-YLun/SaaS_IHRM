d3678f006262ffd728eaf366897a741e6ce0d041
Alan <arvinhan@foxmail.com>
2020-01-05 21:49:43 +0800

    Added 权限的model

---
+++ ihrm_parent/ihrm_common_model/src/main/java/com/ihrm/domain/system/User.java
@@ -0,0 +1,105 @@
+package com.ihrm.domain.system;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 用户实体类
+ */
+@Entity
+@Table(name = "bs_user")
+@Getter
+@Setter
+public class User implements Serializable {
+    private static final long serialVersionUID = 4297464181093070302L;
+    /**
+     * ID
+     */
+    @Id
+    private String id;
+    /**
+     * 手机号码
+     */
+    private String mobile;
+    /**
+     * 用户名称
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 启用状态 0为禁用 1为启用
+     */
+    private Integer enableState;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    private String companyId;
+
+    private String companyName;
+
+    /**
+     * 部门ID
+     */
+    private String departmentId;
+
+    /**
+     * 入职时间
+     */
+    private Date timeOfEntry;
+
+    /**
+     * 聘用形式
+     */
+    private Integer formOfEmployment;
+
+    /**
+     * 工号
+     */
+    private String workNumber;
+
+    /**
+     * 管理形式
+     */
+    private String formOfManagement;
+
+    /**
+     * 工作城市
+     */
+    private String workingCity;
+
+    /**
+     * 转正时间
+     */
+    private Date correctionTime;
+
+    /**
+     * 在职状态 1.在职  2.离职
+     */
+    private Integer inServiceStatus;
+
+    private String departmentName;
+
+
+    /**
+     * 忽略json转化
+     */
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name="pe_user_role",joinColumns={@JoinColumn(name="user_id",referencedColumnName="id")},
+            inverseJoinColumns={@JoinColumn(name="role_id",referencedColumnName="id")}
+    )
+    private Set<Role> roles = new HashSet<Role>();//用户与角色   多对多
+}
---
+++ ihrm_parent/ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
@@ -0,0 +1,31 @@
+package com.ihrm.system;
+
+import com.ihrm.common.utils.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author: hyl
+ * @date: 2020/01/05
+ **/
+//1.配置springboot的包扫描
+@SpringBootApplication(scanBasePackages = "com.ihrm")
+//2.配置jpa注解的扫描
+@EntityScan(value="com.ihrm.domain.system")
+public class SystemApplication {
+
+
+    /**
+     * 启动方法
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
+    }
+}
