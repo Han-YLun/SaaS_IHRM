1185de32d31ec6ff8f0a6c9e4845436be4b67893
Alan <arvinhan@foxmail.com>
2020-02-13 15:04:20 +0800

    Fixed 前端使用iframe下载报表提示未登录

--- ihrm_parent/ihrm_common/src/main/java/com/ihrm/common/utils/DownloadUtils.java
+++ ihrm_parent/ihrm_common/src/main/java/com/ihrm/common/utils/DownloadUtils.java
@@ -13,9 +13,8 @@ public class DownloadUtils {
     public void download(ByteArrayOutputStream byteArrayOutputStream, HttpServletResponse response, String returnName) throws IOException {
         response.setContentType("application/octet-stream");
         returnName = response.encodeURL(new String(returnName.getBytes(),"iso8859-1"));			//保存的文件名,必须和页面编码一致,否则乱码
-        response.addHeader("Content-Disposition","attachment;filename=total.xls");
+        response.addHeader("Content-Disposition","attachment;filename=" + returnName);
         response.setContentLength(byteArrayOutputStream.size());
-        response.addHeader("Content-Length", "" + byteArrayOutputStream.size());
         ServletOutputStream outputstream = response.getOutputStream();	//取得输出流
         byteArrayOutputStream.writeTo(outputstream);					//写到输出流
         byteArrayOutputStream.close();									//关闭
