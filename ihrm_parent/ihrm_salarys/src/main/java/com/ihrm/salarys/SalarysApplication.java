6ed0b07df82d1859b5fed81efa2add445bd53143
Han-YLun <ArvinHan@foxmail.com>
2020-03-17 23:02:03 +0800

    Added 薪资模块以及启动类和Shiro管理器

---
+++ ihrm_parent/ihrm_salarys/src/main/java/com/ihrm/salarys/SalarysApplication.java
@@ -0,0 +1,29 @@
+package com.ihrm.salarys;
+
+import com.ihrm.common.utils.IdWorker;
+import com.ihrm.common.utils.JwtUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(scanBasePackages = "com.ihrm")
+@EntityScan(value = "com.ihrm.domain")
+@EnableFeignClients
+public class SalarysApplication {
+    /**
+     * 启动方法
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SalarysApplication.class, args);
+    }
+
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
+    }
+}
---
+++ ihrm_parent/ihrm_salarys/src/main/java/com/ihrm/salarys/ShiroConfiguration.java
@@ -0,0 +1,127 @@
+package com.ihrm.salarys;
+
+import com.ihrm.common.shiro.realm.IhrmRealm;
+import com.ihrm.common.shiro.session.CustomSessionManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
+import org.crazycake.shiro.RedisSessionDAO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration("ihrm_salarys_shiroConfiguration")
+public class ShiroConfiguration {
+
+    //1.创建realm
+    @Bean
+    public IhrmRealm getRealm() {
+        return new IhrmRealm();
+    }
+
+    //2.创建安全管理器
+    @Bean
+    public SecurityManager getSecurityManager(IhrmRealm realm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(realm);
+
+        //将自定义的会话管理器注册到安全管理器中
+        securityManager.setSessionManager(sessionManager());
+        //将自定义的redis缓存管理器注册到安全管理器中
+        securityManager.setCacheManager(cacheManager());
+
+        return securityManager;
+    }
+
+    //3.配置shiro的过滤器工厂
+
+    /**
+     * 再web程序中，shiro进行权限控制全部是通过一组过滤器集合进行控制
+     *
+     */
+    @Bean
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+        //1.创建过滤器工厂
+        ShiroFilterFactoryBean filterFactory = new ShiroFilterFactoryBean();
+        //2.设置安全管理器
+        filterFactory.setSecurityManager(securityManager);
+        //3.通用配置（跳转登录页面，未授权跳转的页面）
+        filterFactory.setLoginUrl("/autherror?code=1");//跳转url地址
+        filterFactory.setUnauthorizedUrl("/autherror?code=2");//未授权的url
+        //4.设置过滤器集合
+        Map<String,String> filterMap = new LinkedHashMap<>();
+        //anon -- 匿名访问
+        filterMap.put("/sys/login","anon");
+        filterMap.put("/autherror","anon");
+        //注册
+        //authc -- 认证之后访问（登录）
+        filterMap.put("/**","authc");
+        //perms -- 具有某中权限 (使用注解配置授权)
+        filterFactory.setFilterChainDefinitionMap(filterMap);
+
+        return filterFactory;
+    }
+
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+
+    /**
+     * 1.redis的控制器，操作redis
+     */
+    public RedisManager redisManager() {
+        RedisManager redisManager = new RedisManager();
+        redisManager.setHost(host);
+        redisManager.setPort(port);
+        return redisManager;
+    }
+
+    /**
+     * 2.sessionDao
+     */
+    public RedisSessionDAO redisSessionDAO() {
+        RedisSessionDAO sessionDAO = new RedisSessionDAO();
+        sessionDAO.setRedisManager(redisManager());
+        return sessionDAO;
+    }
+
+    /**
+     * 3.会话管理器
+     */
+    public DefaultWebSessionManager sessionManager() {
+        CustomSessionManager sessionManager = new CustomSessionManager();
+        //sessionManager.setSessionIdCookieEnabled(false);
+        sessionManager.setSessionIdUrlRewritingEnabled(false);
+        sessionManager.setSessionDAO(redisSessionDAO());
+        return sessionManager;
+    }
+
+    /**
+     * 4.缓存管理器
+     */
+    public RedisCacheManager cacheManager() {
+        RedisCacheManager redisCacheManager = new RedisCacheManager();
+        redisCacheManager.setRedisManager(redisManager());
+        return redisCacheManager;
+    }
+
+
+
+
+    //开启对shior注解的支持
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(securityManager);
+        return advisor;
+    }
+}
