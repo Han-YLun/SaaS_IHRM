2fc657ba06a64d085dbc3097a8e73ca8df02d0df
Han-YLun <ArvinHan@foxmail.com>
2020-05-29 18:47:04 +0800

    Updated 网关请求判断请求头并且拦截

--- ihrm_parent/ihrm_gate/src/main/java/com/ihrm/gate/filter/LoginFilter.java
+++ ihrm_parent/ihrm_gate/src/main/java/com/ihrm/gate/filter/LoginFilter.java
@@ -1,8 +1,11 @@
 package com.ihrm.gate.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 自定义过滤器
@@ -52,7 +55,21 @@ public class LoginFilter extends ZuulFilter {
      */
     @Override
     public Object run() throws ZuulException {
-        
+        //登录校验逻辑
+        //获取Zuul提供的请求上下文对象
+        RequestContext ctx = RequestContext.getCurrentContext();
+        //从上下文中获取request对象
+        HttpServletRequest req = ctx.getRequest();
+        //从请求中获取token
+        String token = req.getHeader("Authorization");
+        //判断
+        if(token == null || "".equals(token.trim())){
+            //没有token，登录校验失败，拦截
+            ctx.setSendZuulResponse(false);
+            //返回401状态码。也可以考虑重定向到登录页。
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        //校验通过，可以考虑把用户信息放入上下文，继续向后执行
         return null;
     }
 }
